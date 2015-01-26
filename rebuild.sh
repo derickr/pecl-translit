477f5d636bd2222741e28bc9f2ab752af55f0f7b
Derick Rethans <github@derickrethans.nl>
2015-01-26 23:40:09 +0000

    Added the rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export CFLAGS="-Wall -Wextra -Wdeclaration-after-statement -Wmissing-field-initializers -Wshadow -Wno-unused-parameter -ggdb3"
+phpize && ./configure && make clean && make -j 5 all && make install
