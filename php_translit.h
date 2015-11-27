27fb1c6c35f81908945254095c853384fa333a29
Derick Rethans <github@derickrethans.nl>
2015-11-27 11:12:17 +0000

    Go with 0.6.2

--- php_translit.h
+++ php_translit.h
@@ -32,7 +32,7 @@ typedef struct translit_filter_entry {
 extern zend_module_entry translit_module_entry;
 #define phpext_translit_ptr &translit_module_entry
 
-#define PHP_TRANSLIT_VERSION "0.6.0"
+#define PHP_TRANSLIT_VERSION "0.6.2"
 
 #ifdef PHP_WIN32
 #define PHP_TRANSLIT_API __declspec(dllexport)
